9060a98b6279f82f9bcde598bcac0e569aa4feaa
Ramesh <ramesh@rsageventures.com>
2018-10-28 23:09:52 +0530

    Initial Commit

---
+++ src/main/java/com/rs/ssl/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.rs.ssl;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootSslApplication.class);
+	}
+
+}
